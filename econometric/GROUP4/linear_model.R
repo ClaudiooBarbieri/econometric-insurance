d45b80f8025a2f0ca0383faf5e767f84c8bc96ec
ClaudiooBarbieri <claudio.barbieri@mail.polimi.it>
2025-05-30 21:18:58 +0200

    new idea

--- econometric/GROUP4/linear_model.R
+++ econometric/GROUP4/linear_model.R
@@ -1,8 +1,20 @@
 rm(list = ls())
 
+library(ggplot2)
 library(dplyr)
+library(lubridate)
 library(purrr)
-library(tidyr)
+library(patchwork)
+library(GGally)
+library(quantmod) 
+library(TTR)
+library(xts)
+library(IDPmisc)
+library(DescTools)
+library(MASS)
+library(lmtest)
+library(skedastic)
+library(sandwich)
 
 # file with info
 nifty <- read.csv("Nifty50.csv")
@@ -25,4 +37,18 @@ nifty <- nifty %>%
   mutate(log_return = log(close/lag(close))) %>%
   mutate(return = (close - lag(close))/lag(close))
 
+# aggregate at daily level to reduce data noise
+nifty_daily <- nifty %>%
+  group_by(Date) %>%
+  summarise(
+    open = first(open, na.rm = T),
+    high = max(high, na.rm = T),
+    low = min(low, na.rm = T),
+    close = last(close, na.rm = T),
+    volume = sum(volume, na.rm = T),
+    .groups = 'drop'
+  )
 
+nifty_daily <- nifty_daily %>%
+  mutate(log_return = log(close/lag(close))) 
+plot(ts(nifty_daily$log_return))
